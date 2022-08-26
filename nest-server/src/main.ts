5635df2be38ff2ec67620b534b28022781600e82
chechu <ceciaolmedo@gmail.com>
2022-08-26 13:07:14 +0200

    changing cors

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
-import * as cors from 'cors';
+//import * as cors from 'cors';
 
 
 dotenv.config()
@@ -10,7 +10,7 @@ async function bootstrap() {
  
 
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
